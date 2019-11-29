025746754d76ddb39b72d548142e09976e82a3d9
Ashley Rushworth <rushworth.ashley@gmail.com>
2019-11-29 16:11:31 +0000

    Missed in last commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 import behaviors.ExitBehavior;
 import behaviors.MoveBehavior;
+import behaviors.ScanVictimBehavior;
 import behaviors.SelectDestinationBehavior;
 import colourSensorModel.ColourSampleChart;
 import lejos.robotics.subsumption.Arbitrator;
@@ -19,7 +20,6 @@ public class Main {
 	private static final int PORT = 1234; // server port between pc client and robot
 	private static ServerSocket server; // server socket used between robot and pc client.
 	private static boolean useColourChart = true;
-	private static ArrayList<Cell> potentialVictims = new ArrayList<Cell>();
 	private static ArrayList<Cell> route = new ArrayList<Cell>();
 	private static ArrayList<Cell> path = new ArrayList<Cell>();
 	
@@ -67,58 +67,59 @@ public class Main {
 		myMonitor.start();
 		
 		// TODO replace with AgentSpeak logic
-		int bayNumber = 4;
+		int bayNumber = 2;
 		switch (bayNumber) {
 			case 1:
 				grid.getCell(1, 1).setIsBlocked();
 				grid.getCell(1, 4).setIsBlocked();
 				grid.getCell(4, 1).setIsBlocked();
 				grid.getCell(4, 4).setIsBlocked();
-				potentialVictims.add(grid.getCell(0, 5));
-				potentialVictims.add(grid.getCell(2, 0));
-				potentialVictims.add(grid.getCell(2, 2));
-				potentialVictims.add(grid.getCell(2, 4));
-				potentialVictims.add(grid.getCell(5, 4));
+				grid.getCell(0, 5).setStatus(1);
+				grid.getCell(2, 0).setStatus(1);
+				grid.getCell(2, 2).setStatus(1);
+				grid.getCell(2, 4).setStatus(1);
+				grid.getCell(5, 4).setStatus(1);
 				break;
 			case 2:
 				grid.getCell(1, 3).setIsBlocked();
 				grid.getCell(1, 5).setIsBlocked();
 				grid.getCell(2, 4).setIsBlocked();
 				grid.getCell(3, 3).setIsBlocked();
-				potentialVictims.add(grid.getCell(0, 5));
-				potentialVictims.add(grid.getCell(2, 3));
-				potentialVictims.add(grid.getCell(2, 5));
-				potentialVictims.add(grid.getCell(3, 1));
-				potentialVictims.add(grid.getCell(5, 5));
+				grid.getCell(0, 5).setStatus(1);
+				grid.getCell(2, 3).setStatus(1);
+				grid.getCell(2, 5).setStatus(1);
+				grid.getCell(3, 1).setStatus(1);
+				grid.getCell(5, 5).setStatus(1);
 				break;
 			case 3:
 				grid.getCell(2, 2).setIsBlocked();
 				grid.getCell(3, 2).setIsBlocked();
 				grid.getCell(4, 3).setIsBlocked();
 				grid.getCell(5, 0).setIsBlocked();
-				potentialVictims.add(grid.getCell(0, 2));
-				potentialVictims.add(grid.getCell(1, 5));
-				potentialVictims.add(grid.getCell(2, 3));
-				potentialVictims.add(grid.getCell(4, 5));
-				potentialVictims.add(grid.getCell(5, 1));
+				grid.getCell(0, 2).setStatus(1);
+				grid.getCell(1, 5).setStatus(1);
+				grid.getCell(2, 3).setStatus(1);
+				grid.getCell(4, 5).setStatus(1);
+				grid.getCell(5, 1).setStatus(1);
 				break;
 			case 4:
 				grid.getCell(0, 3).setIsBlocked();
 				grid.getCell(1, 2).setIsBlocked();
 				grid.getCell(1, 4).setIsBlocked();
 				grid.getCell(3, 3).setIsBlocked();
-				potentialVictims.add(grid.getCell(0, 2));
-				potentialVictims.add(grid.getCell(0, 4));
-				potentialVictims.add(grid.getCell(1, 3));
-				potentialVictims.add(grid.getCell(4, 3));
-				potentialVictims.add(grid.getCell(5, 5));
+				grid.getCell(0, 2).setStatus(1);
+				grid.getCell(0, 4).setStatus(1);
+				grid.getCell(1, 3).setStatus(1);
+				grid.getCell(4, 3).setStatus(1);
+				grid.getCell(5, 5).setStatus(1);
 		}
 		
 		// set up the behaviours for the arbitrator and construct it
 		Behavior b1 = new MoveBehavior(myRobot, grid, path, csc);
-		Behavior b2 = new SelectDestinationBehavior(pcMonitor, potentialVictims, grid, route, path);
-		Behavior b3 = new ExitBehavior(myRobot, myMonitor, pcMonitor, potentialVictims);
-		Behavior [] behaviorArray = {b1, b2, b3};
+		Behavior b2 = new SelectDestinationBehavior(pcMonitor, grid, route, path);
+		Behavior b3 = new ScanVictimBehavior(myRobot, csc, grid);
+		Behavior b4 = new ExitBehavior(myRobot, myMonitor, pcMonitor, grid);
+		Behavior [] behaviorArray = {b1, b2, b3, b4};
 		Arbitrator arbitrator = new Arbitrator(behaviorArray);
 		arbitrator.go();
 	}
